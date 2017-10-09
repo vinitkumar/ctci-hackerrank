2f31e41b0497d77041dc0da415eaa15437bf0acc
Vineet John <iamvineetjohn@gmail.com>
2017-10-08 22:34:46 -0700

    Completed solution for Strings: Making Anagrams

--- solutions/ctci_making_anagrams.py
+++ solutions/ctci_making_anagrams.py
@@ -1,8 +1,27 @@
 """ Making anagrams """
 
+from collections import Counter
+
+
 def number_needed(string_1, string_2):
     """ Calculates edit distance to make 2 strings anagrams """
-    pass
+
+    multiset_1 = Counter(string_1)
+    multiset_2 = Counter(string_2)
+
+    all_characters = set(string_1) | set(string_2)
+    for character in all_characters:
+        count_1 = multiset_1[character]
+        count_2 = multiset_2[character]
+        multiset_1[character] -= count_2
+        multiset_2[character] -= count_1
+
+    edit_distance = 0
+    for frequency in list(multiset_1.values()) + list(multiset_2.values()):
+        if frequency > 0:
+            edit_distance += frequency
+
+    return edit_distance
 
 
 def main():
@@ -12,6 +31,7 @@ def main():
     string_2 = input().strip()
 
     print(number_needed(string_1, string_2))
+
 
 if __name__ == '__main__':
     main()
