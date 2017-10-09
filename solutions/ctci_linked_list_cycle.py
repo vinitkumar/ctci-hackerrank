7aa8620e6739164f310c01a99771e1ffc4891d2a
Vineet John <iamvineetjohn@gmail.com>
2017-10-09 09:19:09 -0700

    Added solution for Linked Lists: Detect a Cycle

--- solutions/ctci_linked_list_cycle.py
+++ solutions/ctci_linked_list_cycle.py
@@ -1,8 +1,8 @@
 """
 Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
 
-A Node is defined as: 
- 
+A Node is defined as:
+
     class Node(object):
         def __init__(self, data = None, next_node = None):
             self.data = data
@@ -11,5 +11,20 @@ A Node is defined as:
 
 
 def has_cycle(head):
-    pass
-    
+    """ checks if the linked list has a cycle """
+
+    if not head or not head.next:
+        return head
+
+    all_elements = set()
+    current_node = head
+
+    while current_node:
+        if current_node.data in all_elements:
+            return True
+        else:
+            all_elements.add(current_node.data)
+
+        current_node = current_node.next
+
+    return False
