f4870b7b1231475fc63f2310dcf324231141eaa7
Vineet John <iamvineetjohn@gmail.com>
2017-10-11 09:33:45 -0700

    add completed solution for 'Queues: A Tale of Two Stacks'

--- solutions/ctci_queue_using_two_stacks.py
+++ solutions/ctci_queue_using_two_stacks.py
@@ -2,26 +2,34 @@
 
 class MyQueue(object):
     def __init__(self):
-        
-    
-    def peek(self):
-        
-        
-    def pop(self):
-        
-        
-    def put(self, value):
-        
+        self.elements = list()
 
-queue = MyQueue()
-t = int(input())
-for line in range(t):
-    values = map(int, input().split())
-    values = list(values)
-    if values[0] == 1:
-        queue.put(values[1])        
-    elif values[0] == 2:
-        queue.pop()
-    else:
-        print(queue.peek())
-        
+    def peek(self):
+        if self.elements:
+            return self.elements[0]
+
+    def pop(self):
+        if self.elements:
+            popped_element = self.elements[0]
+            self.elements = self.elements[1:]
+
+    def put(self, value):
+        self.elements.append(value)
+
+
+def main():
+    queue = MyQueue()
+    t = int(input())
+    for line in range(t):
+        values = map(int, input().split())
+        values = list(values)
+        if values[0] == 1:
+            queue.put(values[1])        
+        elif values[0] == 2:
+            queue.pop()
+        else:
+            print(queue.peek())
+
+
+if __name__ == '__main__':
+    main()
