e46d8bf1dbf6c5809d30738c655f490510e46119
Vineet John <iamvineetjohn@gmail.com>
2017-10-13 22:15:33 -0700

    add solution for 'DP: Coin Change'

--- solutions/coin_change.py
+++ solutions/coin_change.py
@@ -1,11 +1,32 @@
 ''' DP: Coin Change '''
 
+
 def make_change(coin_denominations, amount):
     '''
     Gets the number of combinations coin_denominations can be used in,
     to create amount
     '''
-    pass
+    coin_denominations = sorted(coin_denominations)
+    dp_matrix = [[1 for _ in range(amount + 1)] for _ in range(len(coin_denominations))]
+
+    for coin_index, coin_denomination in enumerate(coin_denominations):
+        for amount_value in range(amount + 1):
+            if coin_index == 0:
+                if amount_value % coin_denomination == 0:
+                    dp_matrix[coin_index][amount_value] = 1
+                else:
+                    dp_matrix[coin_index][amount_value] = 0
+            elif coin_denomination <= amount_value:
+                dp_matrix[coin_index][amount_value] = \
+                    dp_matrix[coin_index - 1][amount_value] + \
+                    dp_matrix[coin_index][amount_value - coin_denomination]
+            else:
+                dp_matrix[coin_index][amount_value] = dp_matrix[coin_index - 1][amount_value]
+
+    # print(dp_matrix)
+
+    return dp_matrix[len(coin_denominations) - 1][amount]
+
 
 def main():
     ''' Main function '''
