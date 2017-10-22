8928d65be2279ae39716070dc43e074fb05565c2
Vineet John <iamvineetjohn@gmail.com>
2017-10-22 11:54:28 -0700

    add complete solution to 'Time Complexity: Primality'

--- solutions/big_o.py
+++ solutions/big_o.py
@@ -2,14 +2,31 @@
 
 def is_prime(number):
     ''' Checks if a number is prime '''
-    return False
+    upper_bound_check = int(number**(0.5))
+    # print("upper_bound_check:", upper_bound_check)
+
+    for factor in range(2, upper_bound_check + 1):
+        # print("checking factor:", factor)
+        if number % factor == 0:
+            return False
+
+    return True
 
 def main():
     ''' Main function '''
     number_count = int(input().strip())
     for _ in range(number_count):
         number = int(input().strip())
-        print(is_prime(number))
+        print("Prime" if (number != 1 and is_prime(number)) else "Not prime")
+
+def main_test():
+    ''' Main function '''
+    input_file = open("tmp/input09.txt")
+    number_count = int(input_file.readline().strip())
+    for _ in range(number_count):
+        number = int(input_file.readline().strip())
+        print("Prime" if (number != 1 and is_prime(number)) else "Not prime")
 
 if __name__ == '__main__':
     main()
+    # main_test()
