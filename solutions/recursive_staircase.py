0dda09d851832f4cfb443f420088579404fd75a7
Vineet John <iamvineetjohn@gmail.com>
2017-10-22 19:33:45 -0700

    add optimized solution to 'Recursion: Davis' Staircase'

--- solutions/recursive_staircase.py
+++ solutions/recursive_staircase.py
@@ -1,25 +1,36 @@
 ''' Recursion: Davis' Staircase '''
 
+WAYS_TO_CLIMB_CACHE = dict()
+
 def get_ways_to_climb(num_steps_left):
     '''
     Given the step units and the number of steps, return the
     number of ways the staircase can be climbed
     '''
 
+    if num_steps_left in WAYS_TO_CLIMB_CACHE:
+        return WAYS_TO_CLIMB_CACHE[num_steps_left]
+
+    ways_to_climb = 1
+
     if num_steps_left == 0:
-        return 1
+        pass
     elif num_steps_left == 1:
-        return \
+        ways_to_climb = \
             get_ways_to_climb(num_steps_left - 1)
     elif num_steps_left == 2:
-        return \
+        ways_to_climb = \
             get_ways_to_climb(num_steps_left - 1) + \
             get_ways_to_climb(num_steps_left - 2)
     else:
-        return \
+        ways_to_climb = \
             get_ways_to_climb(num_steps_left - 1) + \
             get_ways_to_climb(num_steps_left - 2) + \
             get_ways_to_climb(num_steps_left - 3)
+
+    WAYS_TO_CLIMB_CACHE[num_steps_left] = ways_to_climb
+
+    return ways_to_climb
 
 def main():
     ''' Main function '''
